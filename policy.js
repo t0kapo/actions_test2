229d08fa532f871b5b66df4dd9892f335b87522b
tokap0 <takachin5353@gmail.com>
2021-11-14 04:50:06 +0900

    create new action test

---
+++ policy.js
@@ -0,0 +1,12 @@
+
+const mypolicy = trustedTypes.createPolicy('mypolicy',{
+	createHTML: (unsafe) => {
+		return unsafe
+		  .replace(/&/g, "&amp;")
+      	  .replace(/</g, "&lt;")
+      	  .replace(/>/g, "&gt;")
+      	  .replace(/"/g, "&quot;")
+      	  .replace(/'/g, "&#039;");
+	}
+});
+
